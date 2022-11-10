aca52c23e72bb6f6a9468f65cf2e4bebada31484
Alex Cândido <alex.candido.tec@gmail.com>
2022-11-10 00:10:02 -0300

    obtendo parametros em BookDetails

--- src/pages/BookDetails/index.tsx
+++ src/pages/BookDetails/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 import { BookDetailsContainer } from './styles';
 
 const BookDetails: React.FC = () => {
+  const { id } = useParams();
+
   return <BookDetailsContainer>BookDetails</BookDetailsContainer>;
 };
 
